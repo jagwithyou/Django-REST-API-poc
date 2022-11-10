bfede94987b3e753211cd09815c94ac308f7eb67
Jag <jag@Jags-MacBook-Air.local>
2022-11-10 21:30:10 +0530

    Registered the model to admin

--- AddService/admin.py
+++ AddService/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Ads,Car,UserModel
 # Register your models here.
+
+admin.site.register(UserModel)
+admin.site.register(Car)
+admin.site.register(Ads)
